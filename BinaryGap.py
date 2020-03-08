dc740922695df17473a687b4ab18f205f32f914a
janaina.carvalho <jana271@hotmail.com>
2020-03-07 21:54:25 -0300

    Nova solução para o BinaryGap.

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,39 +1,28 @@
-def solution(n):
-    if n < 5:  # the smallest positive number with binary gap
-        return 0
-
-    nbr_list = list(bin(n))
-    nbr_list.reverse()
-    nbr_trim = str(int(''.join(nbr_list[:-2])))
-    nbr_size = len(nbr_trim)
-
-    if nbr_size < 3:
-        return 0
-
-    count_gaps = 0
-    biggest_gap = 0
-    gap_size = 0
-
-    j = 0
-    while j < nbr_size-1:
-        if nbr_trim[j] == '0' and nbr_trim[j + 1] == '0':
-            gap_size += 1
-        if nbr_trim[j] == '0' and nbr_trim[j + 1] == '1':
-            gap_size += 1
-            count_gaps += 1
-            biggest_gap = gap_size if gap_size > biggest_gap else biggest_gap
-            gap_size = 0
-        j += 1
-    return biggest_gap
-
-
-if __name__ == '__main__':
-    print solution(-4)
-    print solution(0)
-    print solution(1)
-    print solution(2)
-    print solution(3)
-    print solution(20)
-    print solution(55)
-    print solution(1041)
-    print solution(32)
+def solution(N):
+    """
+    Calculate the max gap length within a binary number
+    :param N: decimal number to get the corresponding gap
+    :return: max gap length within the binary number
+    """    
+    if N < 5: return 0
+    
+    # transform decimal number into a string with the 
+    # binary value corresponding without '0b'
+    binary = bin(N)[2:]
+    
+    max_gap = 0
+    gap_len = 0
+    
+    for i in range(len(binary)):
+        n0 = binary[i]
+        try:
+            n1 = binary[i+1]
+        except:
+            n1 = '0'
+        if n0 == '0' and n1 == '0':
+            gap_len +=1
+        elif n0 == '0' and n1 == '1':
+            gap_len += 1
+            max_gap = gap_len if gap_len > max_gap else max_gap
+            gap_len = 0
+    return max_gap
