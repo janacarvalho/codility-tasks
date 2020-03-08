7a72adb8979c0750a51625c8626382996a754b45
janaina.carvalho <jana271@hotmail.com>
2020-03-08 03:47:30 -0300

    Solution for FrogJmp problem.

--- FrogJmp.py
+++ FrogJmp.py
@@ -2,7 +2,12 @@ import math
 
 
 def solution(X, Y, D):
+    """
+    Return the minimal number of jumps from position X 
+    to a position egual or greater than Y.
+    :param X: start position of the frog
+    :param Y: minimal end position
+    :param D: fixed distance of the jump
+    :return: number of jumps to get to a position greater than or equal to Y.
+    """
     return int(math.ceil((Y - X) / float(D)))
-
-
-print solution(10, 85, 30)
